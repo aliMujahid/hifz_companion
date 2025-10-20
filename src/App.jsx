2b385508805c86e8359c6b02c300b3dc67ec6691
mujahid <mhmmdalimujahid@gmail.com>
2025-10-20 11:21:09 +0600

    style the para page and remove bismillah from first ayah of para.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import AyahText from "./components/AyahText";
 import Player from "./components/Player";
+import JuzPage from "./pages/JuzPage";
 import SurahPage from "./pages/SurahPage";
 
 function App() {
   return (
     <>
-      <SurahPage />
-      {/* <Player /> */}
+      {/* <SurahPage /> */}
+      <JuzPage />
     </>
   );
 }
